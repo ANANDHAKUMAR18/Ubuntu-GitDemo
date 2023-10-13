b4be667f366e9cd0d94d6dbf781481167ddd10ff
Anandhakumarg <anandhsandy.g@gmail.com>
2023-10-13 16:18:49 +0530

    This is my first commit for today

---
+++ src/test/java/gitdemo/Maths.java
@@ -0,0 +1,14 @@
+package gitdemo;
+
+public class Maths {
+	public void greet()
+	{
+		System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args) {
+		Maths obj=new Maths();
+		obj.greet();
+	}
+
+}
